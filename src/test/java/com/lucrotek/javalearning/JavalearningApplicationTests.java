c02e1e9ff1937465ae9722699198812e99d4b12e
Eric Lu <xxl3623@gmail.com>
2020-01-18 16:29:28 -0500

    initial commit

---
+++ src/test/java/com/lucrotek/javalearning/JavalearningApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lucrotek.javalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavalearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //tbd
+    }
+
+}
